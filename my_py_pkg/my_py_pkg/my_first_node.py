4d2afc7857f5aaebcfdbce282d19ded51ad7ae5b
AFSALU RAHMAN C <afsalu2002@gmail.com>
2024-12-08 17:22:49 +0530

    4th vommit

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -5,7 +5,6 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
-        self.get_logger().info("Hello ROS2")
         
 def main(args=None):
     rclpy.init(args=args) #initialize/start ros2 communicaion
