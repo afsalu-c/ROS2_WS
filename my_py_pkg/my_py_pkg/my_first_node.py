cc6f14ae61f5573b2782213336c11e96807abfde
AFSALU RAHMAN C <afsalu2002@gmail.com>
2024-12-08 17:29:36 +0530

    added self.get_logger().info("HELLO ROS2")

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -5,6 +5,7 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
+        self.get_logger().info("HELLO ROS2")
         
 def main(args=None):
     rclpy.init(args=args) #initialize/start ros2 communicaion
