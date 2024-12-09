dccb5156772650324a3e7124f76ca1fc567a2bd8
AFSALU RAHMAN C <afsalu2002@gmail.com>
2024-12-09 11:24:25 +0530

    upto minimal cpp node

--- my_py_pkg/my_py_pkg/my_first_node.py
+++ my_py_pkg/my_py_pkg/my_first_node.py
@@ -5,7 +5,13 @@ from rclpy.node import Node
 class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
+        self.counter = 0
         self.get_logger().info("HELLO ROS2")
+        self.create_timer(.5,self.timer_callback)
+
+    def timer_callback(self):
+        self.counter += 1
+        self.get_logger().info("hello" + str(self.counter))
         
 def main(args=None):
     rclpy.init(args=args) #initialize/start ros2 communicaion
